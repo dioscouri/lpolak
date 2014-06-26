0c07470e6211162d7ae35af928ec7da893d31224
Lukas Polak <lpolak@dioscouri.com>
2014-06-26 18:45:24 -0400

    major clean up #1

--- public/index.php
+++ public/index.php
@@ -1,42 +1,37 @@
 <?php
-$app = require('../vendor/bcosca/fatfree/lib/base.php');
+//Require FatFree Base Library https://github.com/bcosca/fatfree
+$app = require '../vendor/bcosca/fatfree/lib/base.php';
 
 $app->set('PATH_ROOT', __dir__ . '/../');
-$app->set('AUTOLOAD',
-        $app->get('PATH_ROOT') . 'lib/;' .
-        $app->get('PATH_ROOT') . 'apps/;'
-);
+$app->set('AUTOLOAD', $app->get('PATH_ROOT') . 'lib/;' . $app->get('PATH_ROOT') . 'apps/;');
 
-require $app->get('PATH_ROOT') . 'vendor/autoload.php';
+//AUTOLOAD all your composer libraries now. 
+(@include_once ($app->get('PATH_ROOT') . 'vendor/autoload.php')) OR die("You need to run php composer.phar install for your application to run.");
 
 $app->set('APP_NAME', 'site');
-if (strpos(strtolower($app->get('URI')), $app->get('BASE') . '/admin') !== false) {
+if (strpos(strtolower($app->get('URI')), $app->get('BASE') . '/admin') !== false)
+{
     $app->set('APP_NAME', 'admin');
 }
 
 // common config
-$app->config( $app->get('PATH_ROOT') . 'config/common.config.ini');
+$app->config($app->get('PATH_ROOT') . 'config/common.config.ini');
 
-// app-specific settings go in the app's config files 
-$app->config( $app->get('PATH_ROOT') . 'config/' . $app->get('APP_NAME') . '.config.ini');
-$app->config( $app->get('PATH_ROOT') . 'config/' . $app->get('APP_NAME') . '.maps.ini');
-$app->config( $app->get('PATH_ROOT') . 'config/' . $app->get('APP_NAME') . '.routes.ini');
-
-$logger = new \Log( $app->get('application.logfile') );
+$logger = new \Log($app->get('application.logfile'));
 \Registry::set('logger', $logger);
 
-if ($app->get('DEBUG')) {
-    ini_set('display_errors',1);
+if ($app->get('DEBUG'))
+{
+    ini_set('display_errors', 1);
 }
 
 // bootstap each mini-app
 \Dsc\Apps::instance()->bootstrap();
-// register all the routes
+
+// load routes
 \Dsc\System::instance()->get('router')->registerRoutes();
 
 // trigger the preflight event
-\Dsc\System::instance()->preflight(); 
+\Dsc\System::instance()->preflight();
 
 $app->run();
-
-?>
