c8578aa1919f6a77711f8d1da523c181e5ed07d4
Lukas Polak <lpolak@dioscouri.com>
2014-07-17 09:22:13 -0400

    Notes part almost done

--- apps/Time/Site/Views/organizations/tab_notes.php
+++ apps/Time/Site/Views/organizations/tab_notes.php
@@ -1,57 +1,63 @@
 <?php 
+	$last_idx = 0;
+	$username = 'Guest';
+	$identity = \Dsc\System::instance()->get('auth')->getIdentity();
+	if (!empty($identity->id)) {
+		$username = $identity->fullName();
+	}
 	$notes = (array) $item->{'notes'};
-	$username = 'elf';
 	
 	echo $this->renderLayout('Time/Site/Views::organizations/fields_add_note.php');
 ?>
 
-<?php for( $i = 0; $i < 4; $i++ ) { ?>
-<div class="panel panel-default" data-element-type="note" data-mode="" data-note-idx="<?php echo $i; ?>">
-  <div class="panel-heading">
-  	<h3 class="panel-title">
-		<span class="note-title"><?php echo $i; ?>Note Title <small>(07/03/2014 20:32 by User)</small></span>
-    	<div class="pull-right" data-element-type="toolbar">
-    		<strong data-element-type="note-status"></strong>
-    		&nbsp;
-			<a class="btn btn-xs btn-secondary" href="javascript:;" data-task="edit">
-				<i class="fa fa-pencil"></i>
-			</a>
-			&nbsp;
-			<a class="btn btn-xs btn-danger" href="javascript:;" data-task="delete">
-				<i class="fa fa-times"></i>
-			</a>
-			<a class="btn btn-xs btn-success" href="javascript:;" data-task="undelete">
-				<i class="fa fa-times"></i>
-			</a>
+<?php if( $c = count( $notes ) ) {
+	$last_idx = $c;
+	for( $idx = $c - 1; $idx > -1; $idx-- ){
+		$note = $notes[$idx];
+	?>
+	<div class="panel panel-default" data-element-type="note" data-mode="" data-note-idx="<?php echo $idx; ?>">
+	  <div class="panel-heading">
+	  	<h3 class="panel-title">
+			<span class="note-title"><?php echo $note['title']; ?> <small>(<?php echo $note['datetime']['local']; ?> by <?php echo $note['user']['name']; ?>)</small></span>
+	    	<div class="pull-right" data-element-type="toolbar">
+	    		<strong data-element-type="note-status"></strong>
+	    		&nbsp;
+				<a class="btn btn-xs btn-secondary" href="javascript:;" data-task="edit">
+					<i class="fa fa-pencil"></i>
+				</a>
+				&nbsp;
+				<a class="btn btn-xs btn-danger" href="javascript:;" data-task="delete">
+					<i class="fa fa-times"></i>
+				</a>
+				<a class="btn btn-xs btn-success" href="javascript:;" data-task="undelete">
+					<i class="fa fa-times"></i>
+				</a>
+				</div>
+	    </h3>
+	  </div>
+	  <div class="panel-body">
+	  	<div data-element-type="original-text"><?php echo $note['description']; ?></div>
+		<div data-type="edit" data-element-type="edit-tools">
+			<div class="form-group">
+				<label>Title</label>
+				<input type="text" class="form-control" id="note_<?php echo $idx; ?>_title"/>
 			</div>
-    </h3>
-  </div>
-  <div class="panel-body">
-  	<div data-element-type="original-text">
-		<?php echo $i; ?>Awesome note with a looot of text. For now!
-  	</div>
-	<div data-type="edit" data-element-type="edit-tools">
-		<div class="form-group">
-			<label>Title</label>
-			<input type="text" class="form-control" id="note_<?php echo $i; ?>_title"/>
+			<div class="form-group">
+				<label>Description</label>
+				<textarea class="form-control" rows="3" id="note_<?php echo $idx; ?>_description"></textarea>
+			</div>
+			<div class="form-group">
+				<button class="btn btn-success" data-task="save">Save</button>
+				<button class="btn btn-danger" data-task="cancel">Cancel</button>
+			</div>
 		</div>
-		<div class="form-group">
-			<label>Description</label>
-			<textarea class="form-control" rows="3" id="note_<?php echo $i; ?>_description"></textarea>
-		</div>
-		<div class="form-group">
-			<button class="btn btn-success" data-task="save">Save</button>
-			<button class="btn btn-danger" data-task="cancel">Cancel</button>
+		<input type="hidden" name="notes[<?php echo $idx; ?>][title]" value="<?php echo $note['title']; ?>" />
+		<input type="hidden" name="notes[<?php echo $idx; ?>][description]" value="<?php echo $note['description']; ?>" />
 		</div>
 	</div>
-	<input type="hidden" name="notes[<?php echo $i; ?>][title]" value="Note Title<?php echo $i; ?>" />
-	<input type="hidden" name="notes[<?php echo $i; ?>][description]" value="<?php echo $i; ?>Awesome note with a looot of text. For now!" />
-	</div>
-</div>
-
-<?php } ?>
-<?php if( count( $notes ) ) { ?>
-<?php } else { ?>
+	
+<?php }
+} else { ?>
 <div class="row">
 	<div class="col-md-12">
 		<div class="alert alert-info">
@@ -63,7 +69,7 @@
 <input name="__notesToDelete" id="notesToDelete" type="hidden" value="" />
 <script type="text/javascript">
 $(function(){
-	Notes.setIndex( <?php echo count( $notes ) + 5 ?> );
+	Notes.setIndex( <?php echo $last_idx; ?> );
 	Notes.setUserName( '<?php echo $username; ?>' );
 	Notes.bindSaveButton( 'button[data-task="save-document"]' );
 });
